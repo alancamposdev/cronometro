dde05286405db690346cd93ff3ba580e4e4a91b1
Alan Campos Linux <camposalan77@gmail.com>
2022-03-09 12:18:33 -0400

    finalizado js

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-let btnStart = document.querySelector('.btn-start');
-let btnPause = document.querySelector('.btn-pause');
-let btnStop = document.querySelector('.btn-stop');
+let tela= document.querySelector('.timer-number');
+let hr = 0;
+let min = 0;
+let seg = 0;
+let intervalo;
 
 function start(){  
-  
+  intervalo = setInterval(watch,100);
 }
 function pause(){
-  
+  clearInterval(intervalo)
 }
 function stop(){
- 
+  clearInterval(intervalo);  
+  seg = 0;
+  min = 0;
+  tela.innerText = `00:00:00`
 }
-function counter(){
- 
+
+  
+function twoDigits(digito){
+  if(digito < 10){
+    return('0'+ digito)
+  }
+  else{
+    return(digito)
+  }
+}
+function watch(){
+  seg++
+  if(seg == 60){
+    min++
+    seg = 0
+    if(min == 60){
+      min= 0
+      hr++
+    }
+  }
+  tela.innerText =`${twoDigits(hr)}:${twoDigits(min)}:${twoDigits(seg)}`
 }
