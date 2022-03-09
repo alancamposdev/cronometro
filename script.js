20c2b7d03a90900ae4a7afb3f23accb62fb6a52c
Alan Campos Linux <camposalan77@gmail.com>
2022-03-09 11:25:23 -0400

    criação da estrutura HTMLeCSS

---
+++ script.js
@@ -0,0 +1,16 @@
+let btnStart = document.querySelector('.btn-start');
+let btnPause = document.querySelector('.btn-pause');
+let btnStop = document.querySelector('.btn-stop');
+
+function start(){  
+  
+}
+function pause(){
+  
+}
+function stop(){
+ 
+}
+function counter(){
+ 
+}
